69cf95c5f0351d09600707e00e8d35301cb4b916
Becky Lee <beckylee.digitaldwp@gmail.com>
2018-09-21 13:54:55 +0100

    changes can now be made

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,66 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration1/frequency', (req, res) => {
+  res.redirect('/iteration1/payment')
+})
+;
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration1/bankdetails', (req, res) => {
+  res.redirect('/iteration1/payment')
+})
+;
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration1/workphone', (req, res) => {
+  res.redirect('/iteration1/overview')
+})
+;
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration1/mobilephone', (req, res) => {
+  res.redirect('/iteration1/overview')
+})
+;
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration1/email', (req, res) => {
+  res.redirect('/iteration1/overview')
+})
+;
+
+
+
 module.exports = router
