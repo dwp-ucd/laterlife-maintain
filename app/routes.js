11181b139fbb52ffb17e46cc27b6816573f1ce32
Becky Lee <beckylee.digitaldwp@gmail.com>
2018-11-27 09:12:34 +0000

    new manual payment prototype

--- app/routes.js
+++ app/routes.js
@@ -151,4 +151,34 @@ router.post('/iteration1/email-remove', function(req, res) {
 });
 
 
+
+// Agent manual -----------------------------------------------------------------
+// ------------------------------------------------------------------------------
+
+router.post('/manualpayment/processaclaim', (req, res) => {
+  res.redirect('/manualpayment/checkclaim')
+})
+;
+
+router.post('/manualpayment/checkclaim', (req, res) => {
+  res.redirect('/manualpayment/suffix')
+})
+;
+
+router.post('/manualpayment/suffix', (req, res) => {
+  res.redirect('/manualpayment/enteramounts')
+})
+;
+
+router.post('/manualpayment/enteramounts', (req, res) => {
+  res.redirect('/manualpayment/payment')
+})
+;
+
+router.post('/manualpayment/payment', (req, res) => {
+  res.redirect('/manualpayment/confirmation')
+})
+;
+
+
 module.exports = router
