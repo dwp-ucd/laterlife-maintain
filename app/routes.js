dbb09acc847fcbaac4e5a457d9abf49ff43fcdc5
Becky <beckylee.digitaldwp@gmail.com>
2019-05-08 12:36:13 +0100

    iteration 5 with payment history and payments made

--- app/routes.js
+++ app/routes.js
@@ -721,6 +721,189 @@ router.post('/iteration4/prevent-payments', function(req, res) {
 
 
 
+///////////////////////////// Iteration 5 ///////////////////////////////
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration5/frequency', (req, res) => {
+  res.redirect('/iteration5/payment')
+})
+;
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration5/bankdetails', (req, res) => {
+  res.redirect('/iteration5/reissue-payment')
+})
+;
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration5/workphone', (req, res) => {
+  res.redirect('/iteration5/overview')
+})
+;
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration5/mobilephone', (req, res) => {
+  res.redirect('/iteration5/overview')
+})
+;
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration5/email', (req, res) => {
+  res.redirect('/iteration5/overview')
+})
+;
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration5/homephone', (req, res) => {
+  res.redirect('/iteration5/overview')
+})
+;
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration5/find', (req, res) => {
+  res.redirect('/iteration5/find-1')
+})
+;
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+router.post('/iteration5/address', (req, res) => {
+  res.redirect('/iteration5/address-1')
+})
+;
+
+router.post('/iteration5/address-1', (req, res) => {
+  res.redirect('/iteration5/homephone-address')
+})
+;
+
+// Change of address and home phone number
+router.post('/iteration5/homephone-address', function(req, res) {
+  if ( req.body['homephone-address'] === 'Yes' ) {
+    res.redirect('homephone');
+  } else {
+    res.redirect('contact');
+  }
+});
+
+// Home phone number removal
+router.post('/iteration5/homephone-remove', function(req, res) {
+  if ( req.body['homephone-remove'] === 'Yes' ) {
+    res.redirect('overview');
+  } else {
+    res.redirect('homephone');
+  }
+});
+
+
+// Mobile phone number removal
+router.post('/iteration5/mobilephone-remove', function(req, res) {
+  if ( req.body['mobilephone-remove'] === 'yes' ) {
+    res.redirect('overview');
+  } else {
+    res.redirect('mobilephone');
+  }
+});
+
+// Work phone number removal & change
+router.post('/iteration5/workphone-remove', function(req, res) {
+  if ( req.body['workphone-remove'] === 'Yes' ) {
+    res.redirect('overview');
+  } else {
+    res.redirect('workphone');
+  }
+});
+
+router.post('/iteration5/workphone', (req, res) => {
+  res.redirect('/iteration5/overview')
+})
+;
+
+// Email removal
+router.post('/iteration5/email-remove', function(req, res) {
+  if ( req.body['email-remove'] === 'yes' ) {
+    res.redirect('overview');
+  } else {
+    res.redirect('email');
+  }
+});
+
+// Marital status change
+router.post('/iteration5/marital-status', (req, res) => {
+  res.redirect('/iteration5/marriage-details')
+})
+;
+
+router.post('/iteration5/marriage-details', (req, res) => {
+  res.redirect('/iteration5/marriage-certificate')
+})
+;
+
+// Contact preferences
+router.post('/iteration5/contact-preferences', (req, res) => {
+  res.redirect('/iteration5/contact')
+})
+;
+
+
+// Stopping payments
+router.post('/iteration5/prevent-payments', function(req, res) {
+  if ( req.body['stopped-reason'] === 'dead' ) {
+    res.redirect('death-date');
+  } else {
+    res.redirect('imprisioned-date');
+  }
+});
+
+
+
 
 ///////////////////////////// BPV iteration ///////////////////////////////////////
 //-------------------------------------------------------------------------------
