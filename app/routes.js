99a160fad651699a75f37ec61cf9887f9b3cfd94
Emily Wilkinson <emilywilkinson2007@hotmail.co.uk>
2019-06-07 11:05:00 +0100

    new claims 3-emily

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 // copy the const name and the file path to new version
 const newclaims = require('./routes/newclaims');
 const newclaims2 = require('./routes/newclaims2');
+const newclaims3 = require('./routes/newclaims3');
 const bpviteration = require('./routes/bpviteration');
 const bpviteration2 = require('./routes/bpviteration2');
 const iteration1 = require('./routes/iteration1');
@@ -15,6 +16,7 @@ const manualpayment = require('./routes/manualpayment');
 //copy the router use and update the sprint version
 router.use(newclaims);
 router.use(newclaims2);
+router.use(newclaims3);
 router.use(bpviteration);
 router.use(bpviteration2);
 router.use(iteration1);
