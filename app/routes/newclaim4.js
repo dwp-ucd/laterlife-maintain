63bc1edf68edb31f3c50315de4642673f2167302
Emily Wilkinson <emilywilkinson2007@hotmail.co.uk>
2020-01-21 07:57:33 +0000

    push

--- app/routes/newclaim4.js
+++ app/routes/newclaim4.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const router = express.Router()
 
-router.use((req, res, next) => {
-    if (req.method === 'POST') {
-      console.log(JSON.stringify(req.session.data, null, 2))
-    }
-    next()
-  })
-  ; 
-
   router.post('/iteration31d/change-status', function(req, res) {
     if ( req.body['maritalstatus'] === 'married' ) {
       res.redirect('marital-details');
