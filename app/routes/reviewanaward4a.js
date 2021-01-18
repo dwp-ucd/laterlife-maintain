3485ff1da081c4d4ce89b262670eb5e297504db6
emilywilkinson <emilywilkinson2007@hotmail.co.uk>
2021-01-18 10:54:38 +0000

    push

--- app/routes/reviewanaward4a.js
+++ app/routes/reviewanaward4a.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.post('/reviewanaward4a/tasks-4-4', function(req, res) {
     if ( req.body['overpayment'] === 'yes' ) {
-      res.redirect('/reviewanaward4a/tasks-5');
+      res.redirect('/reviewanaward4a/tasks-5-5');
     } else {
       res.redirect('/reviewanaward4a/tasks');
     }
