f7d59c3f6d0d4916a9d9f3dd3f2d75f96f106de6
Becky Lee <beckylee.digitaldwp@gmail.com>
2021-06-04 12:18:04 +0100

    Updates to address task

--- app/routes/addresstask.js
+++ app/routes/addresstask.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router()
 
-router.post('/addresstask/address-task-1', function(req, res) {
-if ( req.body['address-fix-1'] === 'Address not found' ) {
-  res.redirect('/addresstask/address-search-1');
-} else {
-  res.redirect('/addresstask/address-matched-1');
-}
-});
+router.post('/addresstask/tasks-1', (req, res) => {
+  res.redirect('/addresstask/address-task-1')
+})
 ;
+
+
+router.post('/addresstask/address-task-1', function(req, res) {
+  if ( req.body['address'] === 'Address not found' ) {
+    res.redirect('address-search-1');
+  } else {
+    res.redirect('tasks-2');
+  }
+});
+
 
 module.exports = router;
