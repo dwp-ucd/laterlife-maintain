27310915ba9446b85d0906522945b9c5494cd806
beckysarahlee <beckylee.digitaldwp@gmail.com>
2021-11-24 14:12:40 +0000

    Routes

--- app/routes/pcsrb2.js
+++ app/routes/pcsrb2.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const router = express.Router()
 
+router.post('/pc-srb-2/review', (req, res) => {
+  res.redirect('/pc-srb-2/review-details')
+})
+;
+
+router.post('/pc-srb-2/review-details', (req, res) => {
+  res.redirect('/pc-srb-2/review-award')
+})
+;
+
+router.post('/pc-srb-2/review-award', (req, res) => {
+  res.redirect('/pc-srb-2/received-pension-credit')
+})
+;
+
   router.post('/pc-srb-2/received-pension-credit', (req, res) => {
     if ( req.body['receiving-pc'] === 'citizen' ) {
       res.redirect('/pc-srb-2/pc-review-task');
