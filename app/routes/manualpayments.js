607e0a2d004d7e9d53cd0fae4623537f589e5434
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-02-02 10:56:25 +0000

    Fixing a messy prototype - again

--- app/routes/manualpayments.js
+++ app/routes/manualpayments.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.post('/manual-payments/returned-payment', function(req, res) {
   if ( req.body['returned'] === 'yes' ) {
-    res.redirect('personal-returned-payment');
+    res.redirect('personal-returned');
   } if ( req.body['returned'] === 'no' ) {
     res.redirect('payment-details-2');
   }
