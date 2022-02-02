36d283aba1c07720b3de54372c585ee59d39c4d9
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-02-02 10:28:47 +0000

    Big clear out

--- app/routes/manualpayments.js
+++ app/routes/manualpayments.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router()
 
+router.post('/manual-payments/returned-payment', function(req, res) {
+  if ( req.body['returned'] === 'yes' ) {
+    res.redirect('personal-returned-payment');
+  } if ( req.body['returned'] === 'no' ) {
+    res.redirect('payment-details-2');
+  }
+});
 
   module.exports = router
