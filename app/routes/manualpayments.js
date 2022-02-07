74f058ee321fac474d7e1c0578d1e8b234e04f8a
kieranwyatt <kieran.wyatt@engineering.digital.dwp.gov.uk>
2022-02-07 15:02:27 +0000

    Manual payments updates

--- app/routes/manualpayments.js
+++ app/routes/manualpayments.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.post('/manual-payments/returned-payment', function(req, res) {
   if ( req.body['returned'] === 'yes' ) {
-    res.redirect('personal-returned');
+    res.redirect('payment-returned');
   } if ( req.body['returned'] === 'no' ) {
     res.redirect('payment-details-2');
   }
