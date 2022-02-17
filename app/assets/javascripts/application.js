5118226d788b836d1160b9c45ae7ff331ce60cd9
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-02-17 16:22:03 +0000

    Revert "Revert "added script for routing on the page""
    This reverts commit 34a50f123d91518104073e462c51829173dd68a6.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,49 @@ $(document).ready(function () {
   }
 })
 
+function nextPageBasedOnSelection($radioObject) {
+  console.log("hello")
+    if (nextPageRoutes) {
+      for (let index = 0; index < Object.keys(nextPageRoutes).length; index++) {
+        var checkboxValue = $radioObject.val()
+        if (nextPageRoutes[checkboxValue] != undefined) {
+          var nextPageField = null
+          if ($('#next-page').length) {
+            nextPageField = $('#next-page')
+          } else {
+            nextPageField = $('<input type="hidden" id="next-page" name="next-page">')
+            $('form').append(nextPageField)
+          }
+          nextPageField.val(nextPageRoutes[checkboxValue])
+          console.log('Next page destination has been set as "' + nextPageRoutes[checkboxValue] + '"')
+          break
+        } else {
+          $('#next-page').remove()
+        }
+      }
+    }
+  
+  }
+  
+  $(document).ready(function () {
+  
+    window.GOVUKFrontend.initAll()
+  
+    try { 
+      $('input[type=radio]:checked').each(function() {
+        nextPageBasedOnSelection($(this))
+      })
+      enableInternalBranding()
+    } catch (e) {}
+  
+  
+  })
+  
+  
+  $('input[type=radio]').on('change', function() {
+    try { 
+      $('input[type=radio]:checked').each(function() {
+        nextPageBasedOnSelection($(this))
+      })
+    } catch (e) {}
+  }) 
