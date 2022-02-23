957317cf03a7397394cb80a9cdccc8619d97fe28
kieranwyatt <kieran.wyatt@engineering.digital.dwp.gov.uk>
2022-02-23 15:10:26 +0000

    Manual payments edits

--- app/routes/manualpayments.js
+++ app/routes/manualpayments.js
@@ -10,4 +10,13 @@ router.post('/manual-payments/returned-payment', function(req, res) {
 
 });
 
+router.post('/manual-payments/reissue-details', function(req, res) {
+  if ( req.body['reissue-payment-1'] === 'yes' ) {
+    res.redirect('reissue-payment');
+  } if ( req.body['reissue-payment-1'] === 'no' ) {
+    res.redirect('payment-details-2');
+  }
+
+});
+
   module.exports = router
