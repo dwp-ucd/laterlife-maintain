df8ad28474da443026da3f417479ac0a08bd356f
Al Robinson <77453029+Al-Robinson@users.noreply.github.com>
2022-03-07 14:26:59 +0000

    iteration 3

--- app/routes/deatharrears.js
+++ app/routes/deatharrears.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router()
 
+router.post('/death-arrears/current/dap-address-1-corporate-dap', function(req, res) {
+  if ( req.session.data ['corp-ref'] === 'corp-ref' ) {
+    res.redirect('corporate-dap-ref-q');
+  }
+
+});
 
   module.exports = router
