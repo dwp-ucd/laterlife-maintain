e785f394acb3d2514770ac190af0564aba5d5c5e
Al Robinson <77453029+Al-Robinson@users.noreply.github.com>
2022-03-11 16:44:44 +0000

    checkbox

--- app/routes/deatharrears.js
+++ app/routes/deatharrears.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router()
 
+router.post('/death-arrears/current/org-dap-send-letter', function(req, res) {
+  if ( req.session.data ['ref'][0] === 'yes' ) {
+    res.redirect('org-dap-send-letter-no-ref');
+  } else {
+    res.redirect('org-dap-send-letter');
+  }
 
+});
 
   module.exports = router
