d7d068f6e3061a06275589095a8e8ed8caf351b9
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-03-15 20:03:59 +0000

    Routes

--- app/routes/identitycheck.js
+++ app/routes/identitycheck.js
@@ -7,6 +7,11 @@ router.post('/identity-check-task/security-check-task-details-record', (req, res
 })
 ;
 
+router.post('/identity-check-task/security-check-task-details', (req, res) => {
+  res.redirect('/identity-check-task/security-check-task')
+})
+;
+
 
 router.post('/identity-check-task/security-check-task', function(req, res) {
   if ( req.body['security-questions'] === 'passed' ) {
@@ -24,7 +29,7 @@ router.post('/identity-check-task/security-check-task-record', function(req, res
   if ( req.body['security-questions'] === 'passed' ) {
     res.redirect('personal-2');
   } else if ( req.body['security-questions'] === 'failed' ) {
-    res.redirect('personal');
+    res.redirect('personal-2');
   } else {
     res.redirect('personal');
   }
