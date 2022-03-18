1d29523ee3afc0cbcd1c67d1c1fb26d7a47c02db
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-03-18 08:51:20 +0000

    Index and routes for identity check 2

---
+++ app/routes/identitycheck2.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router()
+
+
+router.post('/identity-check-task/security-check-task-details-record', (req, res) => {
+  res.redirect('/identity-check-task/security-check-task-record')
+})
+;
+
+router.post('/identity-check-task/security-check-task-details', (req, res) => {
+  res.redirect('/identity-check-task/security-check-task')
+})
+;
+
+
+router.post('/identity-check-task/security-check-task', function(req, res) {
+  if ( req.body['security-questions'] === 'passed' ) {
+    res.redirect('passed');
+  } else if ( req.body['security-questions'] === 'failed' ) {
+    res.redirect('failed');
+  } else if ( req.body['security-questions'] === 'refused' ) {
+    res.redirect('refused');
+  } else {
+    res.redirect('no-answer');
+  }
+});
+
+
+//
+router.post('/identity-check-task/security-check-task-record', function(req, res) {
+  if ( req.body['security-questions'] === 'passed' ) {
+    res.redirect('passed-2');
+  } else if ( req.body['security-questions'] === 'failed' ) {
+    res.redirect('failed-2');
+  } else {
+    res.redirect('refused-2');
+  }
+});
+
+
+
+  module.exports = router;
