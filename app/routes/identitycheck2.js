b0ade675e4249c7a57a6c834d431192db541f7ad
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-03-24 15:42:33 +0000

    Identity check iterations

--- app/routes/identitycheck2.js
+++ app/routes/identitycheck2.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router()
 
 
-router.post('/identity-check-task/security-check-task-details-record', (req, res) => {
-  res.redirect('/identity-check-task/security-check-task-record')
+router.post('/identity-check-task-2/security-check-task-details-record', (req, res) => {
+  res.redirect('/identity-check-task-2/security-check-task-record')
 })
 ;
 
-router.post('/identity-check-task/security-check-task-details', (req, res) => {
-  res.redirect('/identity-check-task/security-check-task')
+router.post('/identity-check-task-2/security-check-task-details', (req, res) => {
+  res.redirect('/identity-check-task-2/security-check-task')
 })
 ;
 
 
-router.post('/identity-check-task/security-check-task', function(req, res) {
+router.post('/identity-check-task-2/security-check-task', function(req, res) {
   if ( req.body['security-questions'] === 'passed' ) {
     res.redirect('passed');
   } else if ( req.body['security-questions'] === 'failed' ) {
@@ -27,7 +27,7 @@ router.post('/identity-check-task/security-check-task', function(req, res) {
 
 
 //
-router.post('/identity-check-task/security-check-task-record', function(req, res) {
+router.post('/identity-check-task-2/security-check-task-record', function(req, res) {
   if ( req.body['security-questions'] === 'passed' ) {
     res.redirect('passed-2');
   } else if ( req.body['security-questions'] === 'failed' ) {
