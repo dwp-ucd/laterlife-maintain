4ce16b9f253bad6907f7f6cdeda33403865d0414
Al Robinson <77453029+Al-Robinson@users.noreply.github.com>
2022-03-28 17:24:32 +0100

    routing

--- app/routes/deatharrears.js
+++ app/routes/deatharrears.js
@@ -10,5 +10,26 @@ router.post('/death-arrears/current/org-dap-ref-change', function(req, res) {
 
 });
 
+router.post('/death-arrears/current/dap-overseas-bank-details', function(req, res) {
+  if ( req.body ['iban'] === 'ES320054285411112222' ) {
+    res.redirect('death-arrears/current/dap-arrears-paid');
+  } else {
+    res.redirect('death-arrears/current/dap-overseas-bank-details-payment');
+  }
+
+});
+
+router.post('/death-arrears/current/dap-overseas-bank-details', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+  if (iban == 'ES320054285411112222') {
+    res.redirect('death-arrears/current/dap-arrears-paid')
+  } else {
+    res.redirect('death-arrears/current/dap-overseas-bank-details-payment')
+  }
+})
+
 
   module.exports = router
+
