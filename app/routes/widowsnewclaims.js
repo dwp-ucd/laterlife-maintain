2ce80beeb1851f55d27a54bac47a8c6cd7ef60aa
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-06-08 15:48:50 +0100

    Widows NC task routing

--- app/routes/widowsnewclaims.js
+++ app/routes/widowsnewclaims.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router()
 
+
+router.post('/widows/new-claims/task-details', (req, res) => {
+  res.redirect('/widows/new-claims/check-details-info-missing')
+})
+;
+
+router.post('/widows/new-claims/check-details-info-missing', function(req, res) {
+    if ( req.body['specialist-decision'] === 'support-queue' ) {
+      res.redirect('/widows/new-claims/get-a-task');
+    } else if  ( req.body['specialist-decision'] === 'more-info' ) {
+      res.redirect('/widows/new-claims/more-info-letter');
+    } else if  ( req.body['specialist-decision'] === 'award' ) {
+      res.redirect('/widows/new-claims/nothing-to-inherit');
+    } else {
+    res.redirect('/widows/new-claims/award-without-inheritance');
+  }
+  });
+
+
 router.post('/widows/new-claims/inheritable', function(req, res) {
     if ( req.body['inheritable-amounts'] === 'yes' ) {
       res.redirect('/widows/new-claims/spouse-spa');
