16f6a44181ae721cc4fcd89a26ac6d278c5b6396
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-06-10 13:17:18 +0100

    Scenario 1

--- app/routes/widowsnewclaims.js
+++ app/routes/widowsnewclaims.js
@@ -52,4 +52,28 @@ router.post('/widows/new-claims/inheritable', function(req, res) {
 
 
 
+// Scenario 1
+
+router.post('/widows/scenario-1/task-details', (req, res) => {
+  res.redirect('/widows/scenario-1/check-details-info-missing')
+})
+;
+
+router.post('/widows/scenario-1/check-details-info-missing', function(req, res) {
+    if ( req.body['specialist-decision'] === 'support-queue' ) {
+      res.redirect('/widows/scenario-1/get-a-task');
+    } else if  ( req.body['specialist-decision'] === 'more-info' ) {
+      res.redirect('/widows/scenario-1/more-info-letter');
+    } else if  ( req.body['specialist-decision'] === 'award' ) {
+      res.redirect('/widows/scenario-1/nothing-to-inherit');
+    } else {
+    res.redirect('/widows/scenario-1/award-without-inheritance');
+  }
+  });
+
+  router.post('/widows/scenario-1/nothing-to-inherit', (req, res) => {
+    res.redirect('/widows/scenario-1/get-a-task-2')
+  })
+  ;
+
 module.exports = router;
