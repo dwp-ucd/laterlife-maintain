77feb12ce4b8a02d460f9050a39eab30b00d573e
Al Robinson <77453029+Al-Robinson@users.noreply.github.com>
2022-06-27 11:53:58 +0100

    .

---
+++ app/routes/dist/deatharrears.dev.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var express = require('express');
+
+var router = express.Router();
+router.post('/death-arrears/current/org-dap-ref-change', function (req, res) {
+  if (req.body['ref'][0] === 'yes') {
+    res.redirect('org-dap-send-letter-no-ref');
+  } else {
+    res.redirect('org-dap-send-letter');
+  }
+}); // router.post('/death-arrears/current/dap-overseas-bank-details', function (req, res) {
+// Get the answer from session data
+// The name between the quotes is the same as the 'name' attribute on the input elements
+// However in JavaScript we can't use hyphens in variable names
+//  if (iban == 'ES320054285411112222') {
+//    res.redirect('death-arrears/current/dap-arrears-paid')
+//  } else {s
+//    res.redirect('death-arrears/current/dap-overseas-bank-details-payment')
+//  }
+// })
+
+router.post('/death-arrears/current/dap-overseas-bank-details', function (req, res) {
+  if (req.body['iban'] === 'ES320054285411112222') {
+    res.redirect('/death-arrears/current/dap-arrears-paid');
+  } else {
+    res.redirect('/death-arrears/current/dap-overseas-bank-details-payment');
+  }
+});
+module.exports = router;
