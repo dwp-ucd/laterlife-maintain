d7fec50b9e91189c743f358ca8ad7784aac197f3
beckysarahlee <beckylee.digitaldwp@gmail.com>
2022-07-04 11:11:06 +0100

    Iteration 2 routes

--- app/routes/debt.js
+++ app/routes/debt.js
@@ -60,6 +60,56 @@ router.post('/debt/confirm-debt', function(req, res) {
 
 
 
+// Iteration two
+
+router.post('/debt/iteration-2/debt-start-date', function(req, res) {
+  if ( req.body['debtstartdate'] === 'manual-date' ) {
+    res.redirect('debt-start-date-manual');
+  } else {
+    res.redirect('debt-details');
+  }
+});
+
+router.post('/debt/iteration-2/debt-edit', function(req, res) {
+  if ( req.body['edit-remove-debt'] === 'remove' ) {
+    res.redirect('remove-confirmation');
+  } else {
+    res.redirect('debt-start-date-manual');
+  }
+});
+
+router.post('/debt/iteration-2/apply-debt', (req, res) => {
+  res.redirect('/debt/confirm-debt')
+})
+;
+
+router.post('/debt/iteration-2/debt-start-date-manual', (req, res) => {
+  res.redirect('/debt/debt-details')
+})
+;
+
+router.post('/debt/iteration-2/debt-details', (req, res) => {
+  res.redirect('/debt/debt-end-date')
+})
+;
+
+
+router.post('/debt/iteration-2/debt-end-date', (req, res) => {
+  res.redirect('/debt/confirm-debt')
+})
+;
+
+router.post('/debt/iteration-2/confirm-debt', function(req, res) {
+  if ( req.session.data['edit-remove-debt'] === 'edit' ) {
+    res.redirect('award-debt-edited');
+  } else {
+    res.redirect('award-updated');
+  }
+});
+
+
+
+
 
 //
 router.post('/', (req, res) => {
