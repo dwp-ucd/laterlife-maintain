f370efdd4ae9e3091c56c27617c1f2a13993fb06
Becky Lee <beckylee.digitaldwp@gmail.com>
2022-10-31 10:13:05 +0000

    Fixing

--- app/routes/pcsrb2.js
+++ app/routes/pcsrb2.js
@@ -1,59 +1,59 @@
 const express = require('express');
 const router = express.Router()
 
-router.post('/pc-srb-2/review', (req, res) => {
-  res.redirect('/pc-srb-2/review-details')
+router.post('/srb/pension-credit/pc-srb-2/review', (req, res) => {
+  res.redirect('/srb/pension-credit/pc-srb-2/review-details')
 })
 ;
-router.post('/pc-srb-2/change-pp', (req, res) => {
-  res.redirect('/pc-srb-2/review-award')
-})
-;
-
-router.post('/pc-srb-2/review-details', (req, res) => {
-  res.redirect('/pc-srb-2/review-award')
+router.post('/srb/pension-credit/pc-srb-2/change-pp', (req, res) => {
+  res.redirect('/srb/pension-credit/pc-srb-2/review-award')
 })
 ;
 
-router.post('/pc-srb-2/change-date', (req, res) => {
-  res.redirect('/pc-srb-2/review-award')
+router.post('/srb/pension-credit/pc-srb-2/review-details', (req, res) => {
+  res.redirect('/srb/pension-credit/pc-srb-2/review-award')
 })
 ;
 
-router.post('/pc-srb-2/change-date2', (req, res) => {
-  res.redirect('/pc-srb-2/review-award2')
+router.post('/srb/pension-credit/pc-srb-2/change-date', (req, res) => {
+  res.redirect('/srb/pension-credit/pc-srb-2/review-award')
 })
 ;
 
-router.post('/pc-srb-2/change-pp2', (req, res) => {
-  res.redirect('/pc-srb-2/review-award2')
+router.post('/srb/pension-credit/pc-srb-2/change-date2', (req, res) => {
+  res.redirect('/srb/pension-credit/pc-srb-2/review-award2')
 })
 ;
 
-router.post('/pc-srb-2/review-award2', (req, res) => {
-  res.redirect('/pc-srb-2/received-pension-credit')
+router.post('/srb/pension-credit/pc-srb-2/change-pp2', (req, res) => {
+  res.redirect('/srb/pension-credit/pc-srb-2/review-award2')
 })
 ;
 
-router.post('/pc-srb-2/change-nsp-2', (req, res) => {
-  res.redirect('/pc-srb-2/review-award2')
+router.post('/srb/pension-credit/pc-srb-2/review-award2', (req, res) => {
+  res.redirect('/srb/pension-credit/pc-srb-2/received-pension-credit')
+})
+;
+
+router.post('/srb/pension-credit/pc-srb-2/change-nsp-2', (req, res) => {
+  res.redirect('/srb/pension-credit/pc-srb-2/review-award2')
 })
 ;
 
 
-router.post('/pc-srb-2/review-award', (req, res) => {
-  res.redirect('/pc-srb-2/received-pension-credit')
+router.post('/srb/pension-credit/pc-srb-2/review-award', (req, res) => {
+  res.redirect('/srb/pension-credit/pc-srb-2/received-pension-credit')
 })
 ;
 
-  router.post('/pc-srb-2/received-pension-credit', (req, res) => {
+  router.post('/srb/pension-credit/pc-srb-2/received-pension-credit', (req, res) => {
     if ( req.body['receiving-pc'] === 'citizen' ) {
-      res.redirect('/pc-srb-2/pc-review-task');
+      res.redirect('/srb/pension-credit/pc-srb-2/pc-review-task');
     }
   else if ( req.body['receiving-pc'] === 'partner' ) {
-    res.redirect('/pc-srb-2/enter-partner-details');
+    res.redirect('/srb/pension-credit/pc-srb-2/enter-partner-details');
   }  else {
-    res.redirect('/pc-srb-2/confirm')
+    res.redirect('/srb/pension-credit/pc-srb-2/confirm')
   }
 })
 
