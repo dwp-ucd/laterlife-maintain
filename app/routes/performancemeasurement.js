d9e31d9b596e676e184f8751b6c057f8772b6616
becky.lee <becky.lee@engineering.digital.dwp.gov.uk>
2023-02-22 12:10:07 +0000

    performance measurement

---
+++ app/routes/performancemeasurement.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router()
+
+router.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.session.data, null, 2))
+  }
+  next()
+})
+
+
+// ITERATION ONE
+
+
+
+  module.exports = router;
