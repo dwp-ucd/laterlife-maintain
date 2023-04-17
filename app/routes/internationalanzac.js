2cec5d92ba12a101e9c32058b983c264583b3e1c
becky.lee <becky.lee@engineering.digital.dwp.gov.uk>
2023-04-17 17:12:22 +0100

    Routes

--- app/routes/internationalanzac.js
+++ app/routes/internationalanzac.js
@@ -19,7 +19,33 @@ router.post('/international-task/anzac-iteration-2/award-improvement', function(
   }
 });
 
+// CFN1282
 
+router.post('/international-task/anzac-cfn1282-iteration-1/cfn-task', function(req, res) {
+  if ( req.body['cfn1282-returned'] === 'yes' ) {
+    res.redirect('date-of-entry');
+  } else {
+    res.redirect('get-a-task-2');
+  }
+});
+
+router.post('/international-task/anzac-cfn1282-iteration-1/date-of-entry', function(req, res) {
+  if ( req.body['date-of-entry'] === 'before' ) {
+    res.redirect('rf1-send');
+  } else {
+    res.redirect('sp-increase');
+  }
+});
+
+router.post('/international-task/anzac-cfn1282-iteration-1/rf1-send', function(req, res) {
+  if ( req.body['rf1'] === 'request' ) {
+    res.redirect('rf1-hmrc');
+  } else if ( req.body['rf1'] === 'received' ) {
+    res.redirect('sp-increase');
+  } else {
+    res.redirect('get-a-task-2');
+  }
+});
 
 
 
