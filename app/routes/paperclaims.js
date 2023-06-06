f8ba52551bb8208425854855331ff254620ea3a0
becky.lee <becky.lee@engineering.digital.dwp.gov.uk>
2023-06-06 10:45:37 +0100

    More prison

--- app/routes/paperclaims.js
+++ app/routes/paperclaims.js
@@ -20,4 +20,44 @@ router.post('/paperclaims/iteration-5c/marital-status', function(req, res) {
     }
   });
 
+
+
+
+// Iteration 9 (POST SPA)
+
+  router.post('/paperclaims/iteration-9/find-1', function(req, res) {
+    if ( req.body['process'] === 'yes' ) {
+      res.redirect('/paperclaims/iteration-9/ni');
+    } else {
+      res.redirect('/paperclaims/iteration-9/drop-out');
+    }
+  });
+
+
+
+  router.post('/paperclaims/iteration-9/ni', (req, res) => {
+    res.redirect('/paperclaims/iteration-9/dob')
+  })
+  ;
+
+  router.post('/paperclaims/iteration-9/dob', (req, res) => {
+    res.redirect('/paperclaims/iteration-9/sp-date')
+  })
+  ;
+
+
+  router.post('/paperclaims/iteration-9/sp-date', (req, res) => {
+    res.redirect('/paperclaims/iteration-9/prison')
+  })
+  ;
+
+
+  router.post('/paperclaims/iteration-9/prison', function(req, res) {
+      if ( req.body['prison'] === 'yes' ) {
+        res.redirect('/paperclaims/iteration-9/prison-details');
+      } else {
+        res.redirect('/paperclaims/iteration-9/contacted');
+      }
+    });
+
 module.exports = router;
